3af4756d510344e1bfa5050de38e95dc2cd39dc7
zs-akkaya <zsakkaya8@gmail.com>
2024-12-21 15:39:16 +0300

    MongoDB connection made, Server.js created

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// CORS Middleware (Cross Origin Resource Sharing) (Authorized resource sharing)
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/restaurantDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
